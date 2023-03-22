abe4522d0ea47788e25e30e1d74e7c8788950149
jude iwuji <judeiwuji@gmail.com>
2023-03-22 09:08:44 +0100

    Add: Task 0

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    idx = 0
+
+    for i in range(1, n):
+        prev = triangle[idx]
+        row = [1]
+        for j in range(1, len(prev)):
+            row.append(prev[j-1] + prev[j])
+        row.append(1)
+        idx += 1
+        triangle.append(row)
+    return triangle
