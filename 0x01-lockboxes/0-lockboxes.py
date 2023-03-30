7bcbef81eca86be5e3ffe11570627c90ed8a97d7
judeiwuji <judeiwuji@gmail.com>
2023-03-30 13:53:25 -0700

    Add: Task 0

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module:  Lockboxes"""
+
+
+def canUnlockAll(boxes):
+    """Check if all boxes can be unlocked
+
+    Args:
+        boxes:(list)
+    """
+    unlocked = {}
+    unlockCount = 0
+
+    for i, box in enumerate(boxes):
+        if len(box) == 0 and i + 1 == len(boxes):
+            unlockCount += 1
+        for key in box:
+            if key < len(boxes):
+                door = unlocked.get(key, None)
+                if door == None:
+                    unlocked[key] = key
+                    unlockCount += 1
+    # print(unlocked)
+    return unlockCount == len(boxes)
