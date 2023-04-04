d8d34e42c09ab5f5975ea6fc3c768cda1c153bf9
judeiwuji <judeiwuji@gmail.com>
2023-04-04 07:35:27 -0700

    Fixed errors

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,7 +3,7 @@
 
 
 def minOperations(n):
-    """calculates the fewest number of operations needed to 
+    """calculates the fewest number of operations needed to
     result in exactly `n` `H` characters in the file"""
 
     ops = {'copyAll': 0, 'paste': 0}
