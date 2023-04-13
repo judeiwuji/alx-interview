b7df37d71df857cd33900b3ed69af0af62e361bc
judeiwuji <judeiwuji@gmail.com>
2023-04-13 05:02:33 -0700

    update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,12 +7,12 @@ db = {}
 pattern = r"^(\d+(\.\d+){3})\s\-\s(\[\d+(\-\d+){2}\s\d+(\:\d+\.?(\d+)?)" +\
     r"{2}\])\s(\"GET \/projects\/260 HTTP\/1.1\")" +\
     r"\s(200|301|400|401|403|404|405|500)\s(\d+)$"
-file_size = 0
+total_size = 0
 count = 0
 
 
 def print_stats():
-    print("File size: {:d}".format(file_size))
+    print("File size: {:d}".format(total_size))
     for key, value in sorted(db.items()):
         print("{}: {:d}".format(key, value))
 
@@ -33,11 +33,12 @@ if __name__ == "__main__":
                 if db.get(status) is None:
                     db[status] = 0
                 db[status] += 1
-                file_size += int(size)
+                total_size += int(size)
 
-                if count >= 10:
-                    print_stats()
-                    count = 0
+            if count == 10:
+                print_stats()
+                count = 0
     except KeyboardInterrupt:
         print_stats()
+        sys.stderr.flush()
         traceback.print_exc()
