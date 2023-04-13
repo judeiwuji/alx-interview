5f0a67416e1b52722ccc29a2313a9a1c34e84beb
judeiwuji <judeiwuji@gmail.com>
2023-04-13 02:25:49 -0700

    Add: Task 0

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Module: Log statistics"""
+import fileinput
+import re
+db = {}
+pattern = r"^(\d+(\.\d+){3})\s\-\s(\[\d+(\-\d+){2}\s\d+(\:\d+\.?(\d+)?)" +\
+    r"{2}\])\s(\"GET \/projects\/260 HTTP\/1.1\")" +\
+    r"\s(200|301|400|401|403|404|405)\s(\d+)$"
+file_size = 0
+count = 0
+
+
+def print_stats():
+    print("File size: {:d}".format(file_size))
+    for key, value in sorted(db.items()):
+        print("{}: {:d}".format(key, value))
+
+
+if __name__ == "__main__":
+    for line in fileinput.input():
+        count += 1
+        log = line.rstrip()
+        matches = re.findall(pattern, line)
+
+        if matches is not None and len(matches) > 0:
+            matches = list(matches[0])
+            ip = matches[0]
+            status = matches[-2]
+            size = matches[-1]
+
+            if db.get(status) is None:
+                db[status] = 0
+            db[status] += 1
+            file_size += int(size)
+
+            if count == 10:
+                print_stats()
+                count = 0
