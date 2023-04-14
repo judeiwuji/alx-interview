4a945a3452571f608e195889bd708ee1308d29ad
judeiwuji <judeiwuji@gmail.com>
2023-04-14 14:05:38 -0700

    update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,7 +4,8 @@ import re
 import sys
 import traceback
 db = {}
-pattern = r"^((\d+(\.\d+){3})|\w+?)\s?\-\s?(\[\d+(\-\d+){2}\s\d+(\:\d+\.?(\d+)?)" +\
+pattern = r"^((\d+(\.\d+){3})|\w+?)\s?\-\s?" +\
+    r"(\[\d+(\-\d+){2}\s\d+(\:\d+\.?(\d+)?)" +\
     r"{2}\])\s(\"GET \/projects\/260 HTTP\/1.1\")" +\
     r"\s(\w+)\s(\d+)$"
 statusCodes = [200, 301, 400, 401, 403, 404, 405, 500]
