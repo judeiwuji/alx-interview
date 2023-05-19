9af3cbdc9f2728316858c66097c17b8457bbcd4e
jude iwuji <judeiwuji@gmail.com>
2023-05-19 21:09:30 +0100

    Add: Task 0

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module: makeChange"""
+def makeChange(coins, total):
+    """determine the fewest number of coins needed to
+    meet a given amount total
+
+    Args:
+     coins: list(int)
+     total: int
+    """
+    d = coins[:]
+    totalChange = 0;
+    count = 0;
+    while totalChange < total and len(d) > 0:
+       change = max(d)
+       if change + totalChange <= total:
+           totalChange += change
+           count += 1
+       else:
+           d.remove(change)
+    return count if totalChange == total else -1
